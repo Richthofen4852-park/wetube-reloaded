a0105bd7c005f259c3237df927a3f38da83cdfab
Richthofen4852-park <smash87299@gmail.com>
2022-12-08 06:35:45 +0900

    External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
-  next();
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hello");
 }
 
-app.get("/", gossipMiddleware, (req, res) => {
-  return res.send("I`m love middlewares");
-});
+const login = (req, res) => {
+  return res.send("login");
+}
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}!`);
 
