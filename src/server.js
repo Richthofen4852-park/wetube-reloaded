fa15b22678e474d27227bf6c2e4c1f0adaae4f63
Richthofen4852-park <smash87299@gmail.com>
2022-12-07 19:06:57 +0900

    Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,8 +4,13 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.end();
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+}
+
+app.get("/", gossipMiddleware, (req, res) => {
+  return res.send("I`m love middlewares");
 });
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}!`);
