b91a2856d19f9681a69f85c0d9db9c817cbab63b
Richthofen4852-park <smash87299@gmail.com>
2022-12-07 18:43:15 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.end();
+});
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}!`);
+
+app.listen(PORT, handleListening)
