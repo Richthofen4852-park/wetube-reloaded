481df5751ffab270d3953d8b73c3e29d84933c48
Richthofen4852-park <smash87299@gmail.com>
2022-12-23 11:00:46 +0900

    Search part two

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { join } from "../controllers/userController";
-import { home } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 import { login } from "../controllers/userController";
 
 const globalRouter = express.Router();
@@ -8,6 +8,6 @@ const globalRouter = express.Router();
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-
+globalRouter.get("/search", search)
 
 export default globalRouter;
